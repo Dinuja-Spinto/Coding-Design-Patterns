5befd51e2e3a9902f5a5ea6f6d4e97d8f4897f89
Dinuja Pinto <sri.prabasha@gmail.com>
2023-10-31 20:14:05 +0530

    BitwiseXOR

---
+++ src/codingPatterns/BitwiseXOR.java
@@ -0,0 +1,11 @@
+package codingPatterns;
+public class BitwiseXOR {
+    public static void main(String[] args) {
+        int num1 = 22;  // Binary: 10110
+        int num2 = 27;  // Binary: 11011
+
+        int result = num1 ^ num2;
+
+        System.out.println("Result of bitwise XOR: " + result);
+    }
+}
